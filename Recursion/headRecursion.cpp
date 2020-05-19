5925adcf3b11540687e8ce82b16d8b45c5024948
michu_janczyk <michu.janczyk@gmail.com>
2020-05-19 22:14:56 +0200

    Recursion init

---
+++ Recursion/headRecursion.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// Head Recursion
+void fun(int n)
+{
+    if (n > 0)
+    {
+        fun(n - 1);
+        printf("%d ", n);
+    }
+}
+
+int main()
+{
+    int x = 3;
+    fun(x);
+    return 0;
+}
